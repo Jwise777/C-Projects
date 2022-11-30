1d2098d768370d0078021047ef3a736c6b0cbe50
Jwise777 <109560319+Jwise777@users.noreply.github.com>
2022-11-29 22:57:05 -0500

    boolean

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Car Insurance Application");
+            Console.ReadLine();
+
+            Console.WriteLine("Please answer the following questions for approval:");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int driveAge = Convert.ToInt32(age);
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string everDui = Console.ReadLine();
+            bool dui = Convert.ToBoolean(everDui);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedTickets = Console.ReadLine();
+            int tickets = Convert.ToInt32(speedTickets);
+            Console.ReadLine();
+
+            Console.WriteLine("Qualified?");
+            Console.ReadLine();
+            bool canInsure = (driveAge >= 15 && false dui && < 3 tickets);
+            Console.WriteLine(canInsure);
+            Console.ReadLine();
+        }
+    }
+}
