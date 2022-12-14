a7f2798ba4de938aa8f0636d3c78869325024740
Jwise777 <109560319+Jwise777@users.noreply.github.com>
2022-12-13 23:37:05 -0500

    mathmethod2

---
+++ MainMethodApp279/MainMethodApp279/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp279
+{
+    class Math
+    {
+        public int mathMethod(int x)
+        {
+            x = 10 * x;
+            return x;
+        }
+
+        public int mathMethod(decimal x)
+        {
+            int y = Convert.ToInt32(x) - 5;
+            return y;
+        }
+
+        public int mathMethod(string x)
+        {
+            int z = Convert.ToInt32(x) + 5;
+            return z;
+        }
+    }
+}
---
+++ MainMethodApp279/MainMethodApp279/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp279
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.mathMethod(10);
+            math.mathMethod(5.5m);
+            math.mathMethod("10");
+            Console.ReadLine();
+        }
+    }
+}
